83dac437ae405ae7aef11eacdc79c72b7c15a707
abdollah nouri <46811451+a-nouri@users.noreply.github.com>
2024-03-05 19:24:11 +0330

    Update ModelGeneratrorMySqlMormot.sql

--- ModelGeneratrorMySqlMormot.sql
+++ ModelGeneratrorMySqlMormot.sql
@@ -1,5 +1,5 @@
-select t.table_name,concat('published\r\n',
-group_concat(col order by ordinal_position separator ';\r\n'),';') table_create from (
+select t.table_name,concat('  published\r\n',
+group_concat('    ',col order by ordinal_position separator ';\r\n'),';') table_create from (
 select a.table_name,a.ordinal_position,concat('property ',a.column_name,': ',
 if(a.data_type in ('varchar','enum','text','char','time','varbinary','tinytext','set','mediumtext'),
 if(a.is_nullable='YES','TNullableUtf8Text','RawUtf8'),
@@ -18,5 +18,5 @@ if(a.data_type in ('varchar'),concat(' index ',a.character_maximum_length),''),
 ' read F',a.column_name,' write ',
 if(a.data_type='enum',concat('set',a.column_name),concat('F',a.column_name))
 ) col from information_schema.columns a
-where table_schema='passak_utf') t
+where table_schema='your_database_name') t
 group by t.table_name
